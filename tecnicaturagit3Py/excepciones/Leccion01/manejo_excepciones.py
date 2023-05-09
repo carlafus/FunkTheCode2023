410c10f302e1ca489950a173ef8c01dc301fec63
Andrés Montes <m.andresmontes@gmail.com>
2023-05-09 08:28:31 -0300

    Excepciones
    Manejo de excepciones en Python

--- tecnicaturagit3Py/excepciones/Leccion01/manejo_excepciones.py
+++ tecnicaturagit3Py/excepciones/Leccion01/manejo_excepciones.py
@@ -22,3 +22,39 @@ finally: # siempre se va a ejecutar
 
 print(f'el resultado es: {resultado}')
 print('seguimos...')
+
+
+""""
+#codigo Andy
+from NumerosIgualesException import NumerosIgualesException
+resultado = None
+
+resultado = None
+
+try:
+    a = int (input('Digite el primer número: '))
+    b = int (input('Digite el primer número: '))
+
+    if a == b:
+        raise NumerosIgualesException('Son números iguales')
+    resultado = a / b
+except TypeError as e:
+    print(f'TypeError - ocurrió un error: {type(e)}')
+
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrió un error {type (e)}')
+
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type (e)}')
+
+else:
+    print(f'No se arrojo ninguna excepción')
+
+finally:
+    print("Ejecución de este bloque finally")
+
+print(f'El resultado es {resultado}')
+print('seguimos...') 
+
+#prueba de GIT FORK
+"""
