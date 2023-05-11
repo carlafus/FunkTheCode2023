9dd649681accf2a3d4d29816a7594dc0192c079c
jonathan catriel quintana <111572681+catrielquin@users.noreply.github.com>
2023-05-11 15:39:25 -0300

    Add files via upload
    agrego lo visto en la semana 5 de python

--- tecnicaturagit3Py/Leccion04/BD/prueba_BD.py
+++ tecnicaturagit3Py/Leccion04/BD/prueba_BD.py
@@ -7,12 +7,18 @@ conexion = psycopg2.connect(
     port='5432',
     database='test_bd'
 )
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s'
+            id_persona = input("digite un numero para el id_persona: ")
+            cursor.execute(sentencia,(id_persona,)) # de esta manera ejecutamos la sentencia
+            registros = cursor.fetchone() # recuperamos todos los registros que seran una lista
+            print(registros)
 
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) # de esta manera ejecutamos la sentencia
-registros = cursor.fetchall() # recuperamos todos los registros que seran una lista
-print(registros)
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
 
-cursor.close()
-conexion.close()
+# https_//www.psycopg.org/focs/usage.html
