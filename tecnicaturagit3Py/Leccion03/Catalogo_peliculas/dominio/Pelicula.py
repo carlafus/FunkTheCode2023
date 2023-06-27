6881fc68adfd4b50f0f7aa91eca21a446e4643a2
carla fuschino <39710178+carlafus@users.noreply.github.com>
2023-06-26 22:06:24 -0300

    Update Pelicula.py

--- tecnicaturagit3Py/Leccion03/Catalogo_peliculas/dominio/Pelicula.py
+++ tecnicaturagit3Py/Leccion03/Catalogo_peliculas/dominio/Pelicula.py
@@ -11,24 +11,3 @@ class Pelicula:
     @nombre.setter
     def nombre(self, nombre):
         self._nombre = nombre
-
-
-
-#---------------------------------------------------------------------------
-
-''' codigo matias:
-class Pelicula:
-    def __init__(self, nombre):
-        self._nombre = nombre
-
-    def __str__(self):
-        return f'Pelicula: {self._nombre}'
-
-    @property
-    def nombre(self):
-        return self._nombre
-
-    @nombre.setter
-    def nombre(self, nombre):
-        self._nombre = nombre
-    '''
