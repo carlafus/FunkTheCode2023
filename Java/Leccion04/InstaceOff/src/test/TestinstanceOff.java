0ce35e59f40b91fe539caa75ee1590eb8f0b24d4
DaniloProfita <111090713+DaniloProfita@users.noreply.github.com>
2023-06-26 23:26:56 -0300

    TestinstanceOff.java
    Semana 4 - Java - 1.5 Instance of Parte 1 y 2

--- Java/Leccion04/InstaceOff/src/test/TestinstanceOff.java
+++ Java/Leccion04/InstaceOff/src/test/TestinstanceOff.java
@@ -1,32 +1,30 @@
-
 package test;
 
 import domain.*;
 
-
-public class TestinstanceOff {
+public class TestInstanceOf {
+    private String departamento;
+    
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Juan", 10000);
         determinarTipo(empleado1);
         empleado1 = new Gerente("Jose", 5000, "Sistemas");
-        //determinarTipo(empleado1); 
-    }
+        determinarTipo(empleado1);
+    }    
     
     public static void determinarTipo(Empleado empleado){
-        if (empleado instanceof Gerente){
-            System.out.println("Es de tipo gerente");
-           // Gerente gerente = (Gerente)empleado;
-            ((Gerente) empleado) .getDepartamento();
-            System.out.println("gerente = "+ empleado);                 
-        }
-        else if(empleado instanceof Empleado){
+        if(empleado instanceof Gerente){
+            System.out.println("Es de tipo Gerente");
+            //((Gerente) empleado).getDepartamento();
+            //System.out.println("gerente ="+empleado);
+            Gerente gerente = (Gerente)empleado;
+            System.out.println("gerente = "+gerente.getDepartamento());
+        }else if(empleado instanceof Empleado){
             System.out.println("Es de tipo Empleado");
-          //  Gerente gerente = (Gerente)empleado;
-          //  System.out.println("gerente = "+ gerente.getDepartamento());
-        }
-        else if(empleado instanceof Object){
-            System.out.println("es de tipo object");
-            
+            //Gerente gerente = (Gerente)empleado;
+            //System.out.println("gerente = "+gerente.getDepartamento());
+        }else if(empleado instanceof Object){
+            System.out.println("Es de tipo Object");
         }
     }
 }
