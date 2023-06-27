b80ba5ad17879ca4dee379014237dd1e7f70e532
DaniloProfita <111090713+DaniloProfita@users.noreply.github.com>
2023-06-26 22:25:25 -0300

    actualizar_varios_reg.py
    Semana 5 - Python - 5.7 Actualizar o modificar varios registros

--- tecnicaturagit3Py/Leccion04/BD/actualizar_varios_reg.py
+++ tecnicaturagit3Py/Leccion04/BD/actualizar_varios_reg.py
@@ -1,21 +1,18 @@
-import psycopg2 # esto es para poder conectarnos a postgre
+import psycopg2
 
-conexion = psycopg2.connect(
-    user = 'postgres',
-    password='Manaos.22',
-    host='127.0.0.1',
-    port='5432',
-    database='test_bd'
-)
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = "DELETE FROM persona WHERE id_persona IN %s"
-            entrada = input("digite ls numeros de registros a eliminar(separados por coma) : ")
-            valores = (entrada,) # es una tupla de valores
-            cursor.execute(sentencia, valores) # de esta manera ejecutamos la sentencia
-            registros_eliminados = cursor.rowcount
-            print(f"los registros eliminados son:{registros_eliminados}")
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = (
+                ('Juan', 'Perez', 'jperez@mail.com', 4),
+                ('Romina', 'Ayala', 'rayala@mail.com', 5)
+            )
+            cursor.executemany(sentencia, valores)
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son : {registros_actualizados}')
 
 except Exception as e:
     print(f'Ocurrio un error: {e}')
