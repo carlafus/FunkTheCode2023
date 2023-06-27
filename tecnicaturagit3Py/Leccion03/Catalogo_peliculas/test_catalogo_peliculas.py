3da9e4928acfbe031a9b21b5896db17e20ad7649
Bárbara Herrera <114678422+Lenovitch@users.noreply.github.com>
2023-06-26 23:41:04 -0300

    Update test_catalogo_peliculas.py
    Semana 3 - Creación de: test_catalogo_peliculas

--- tecnicaturagit3Py/Leccion03/Catalogo_peliculas/test_catalogo_peliculas.py
+++ tecnicaturagit3Py/Leccion03/Catalogo_peliculas/test_catalogo_peliculas.py
@@ -1,25 +1,25 @@
 from dominio.Pelicula import Pelicula
-from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+from Servicio.catalogo_peliculas import CatalogoPeliculas as cp
+
 opcion = None
 while opcion != 4:
     try:
         print('Opciones: ')
         print('1. Agregar Pelicula')
-        print('2. Listar las Peliculas')
-        print('3. Eliminar Catalogo de Peliculas')
+        print('2. Listar las peliculas')
+        print('3. Eliminar catálogo de películas')
         print('4. Salir')
-        opcion = int(input('Digite una opción de menú (1-4): '))
+        opcion = int(input('Digite una opción de menú(1-4): '))
         if opcion == 1:
-            nombre_pelicula = input('digite el nombre de la pelicula: ')
+            nombre_pelicula = input('Digite el nombre de la película: ')
             pelicula = Pelicula(nombre_pelicula)
             cp.agregar_peliculas(pelicula)
-        elif opcion == 2:
+        if opcion == 2:
             cp.listar_peliculas()
         elif opcion == 3:
             cp.eliminar_peliculas()
     except Exception as e:
-        print(f'Ocurrio un error de tipo: {e}')
+        print(f'Ocurrió un error de tipo: {e}')
         opcion = None
     else:
-        print('Salimos del Programa')
-
+        print('Salimos del programa')
