8103fc91644e950bc9c9436d6e5464512c69004d
carla fuschino <39710178+carlafus@users.noreply.github.com>
2023-06-26 22:03:33 -0300

    Update Pelicula.py
    Creacion de la clase catalogo_ pelicula

--- tecnicaturagit3Py/Leccion03/Catalogo_peliculas/dominio/Pelicula.py
+++ tecnicaturagit3Py/Leccion03/Catalogo_peliculas/dominio/Pelicula.py
@@ -6,9 +6,29 @@ class Pelicula:
         return f'Pelicula: {self._nombre}'
 
     @property
+    def nombre (self):
+        return self._nombre
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+
+
+#---------------------------------------------------------------------------
+
+''' codigo matias:
+class Pelicula:
+    def __init__(self, nombre):
+        self._nombre = nombre
+
+    def __str__(self):
+        return f'Pelicula: {self._nombre}'
+
+    @property
     def nombre(self):
         return self._nombre
 
     @nombre.setter
     def nombre(self, nombre):
         self._nombre = nombre
+    '''
