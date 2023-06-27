bb5a9bb152b2abb16b13af3f1394ab301ca9150b
DaniloProfita <111090713+DaniloProfita@users.noreply.github.com>
2023-06-26 23:55:10 -0300

    CalculadoraUTN.java
    Semana 9 - Java - 9.3 Ponemos la entrada de información

--- Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,68 +1,74 @@
 import java.util.Scanner;
+
 public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        while (true){// ciclo infinito
-
-            System.out.println("************** Aplicacion Calculadora***************");
+        while (true){ //Ciclo infinito
+            System.out.println("****** Aplicacion Calculadora ******");
+            System.out.println();
             mostrarMenu();
 
-            try {
+            try{
                 var operacion = Integer.parseInt(entrada.nextLine());
-                if (operacion >= 1 && operacion <= 4) {
-
+                if(operacion >= 1 && operacion <= 4){
                     ejecutarOperacion(operacion, entrada);
-                } // fin del if
-                else if (operacion == 5) {
-                    System.out.println("hasta pronto...");
-                    break; // rompe el ciclo
-                } else {
-                    System.out.println("Opcion erronea: " + operacion);
+                } //Fin del if
+                else if(operacion == 5){
+                    System.out.println("Hasta pronto...");
+                    break; //Rompe el ciclo while y sale
                 }
-                //imprimimos un salto de linea antes de repetir el menu
+                else {
+                    System.out.println("Opcion erronea: "+operacion);
+                }
+                //Imprimimos un salto de linea antes de repetir el menu
                 System.out.println();
-            } catch (Exception e) { // fin del try, comienzo del catch
-                System.out.println("Ocurrio un error: " + e.getMessage());
+                System.out.println();
+            } catch (Exception e){ //Fin try, comienza del catch
+                System.out.println("Ocurrio un error: "+e.getMessage());
+                System.out.println();
                 System.out.println();
             }
-        }//fin while
-    } // fin del main
+        } //Fin while
+    } //Fin main
 
-    private  static void mostrarMenu(){
+    private static void mostrarMenu(){
         //Mostramos el menu
         System.out.println("""
-                    1. Suma
-                    2. Resta
-                    3. Multiplicacion
-                    4. Division
-                    5. Salir             
-                    """);
-        System.out.println("Operacion a realizar");
-    } // fin metodo mostrar menu
-    private static  void ejecutarOperacion(int operacion, Scanner entrada){
-        System.out.println("Digite el valor para el operando1: ");
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        System.out.print("Operacion a realizar? ");
+    }//Fin de mostrarMenu()
+
+    private static void ejecutarOperacion(int operacion, Scanner entrada){
+        System.out.print("Digite el valor para el operando1: ");
         var operando1 = Double.parseDouble(entrada.nextLine());
-        System.out.println("Digite el valor para el operando2: ");
+
+        System.out.print("Digite el valor para el operando2: ");
         var operando2 = Double.parseDouble(entrada.nextLine());
-        Double resultado;
-        switch (operacion) {
-            case 1 -> { // suma
+
+        double resultado;
+        switch (operacion){
+            case 1 -> {
                 resultado = operando1 + operando2;
-                System.out.println("Resultado de la suma: " + resultado);
+                System.out.println("Resultado de la suma: "+resultado);
             }
-            case 2 -> { // resta
+            case 2 -> {
                 resultado = operando1 - operando2;
-                System.out.println("Resultado de la resta: " + resultado);
+                System.out.println("Resultado de la resta: "+resultado);
             }
-            case 3 -> { // multiplicacion
+            case 3 -> {
                 resultado = operando1 * operando2;
-                System.out.println("Resultado de la multiplicacion: " + resultado);
+                System.out.println("Resultado de la multiplicacion: "+resultado);
             }
-            case 4 -> { // division
+            case 4 -> {
                 resultado = operando1 / operando2;
-                System.out.println("Resultado de la division: " + resultado);
+                System.out.println("Resultado de la division: "+resultado);
             }
-            default -> System.out.println("Opcion erronea: " + operacion);
-        }// fin switch
-    }// fin metodo ejecutar
-} // fin clase
+            default -> System.out.println("Opcion erronea: "+operacion);
+        } //Fin switch
+    }//Fin ejecutarOperacion()
+}//Fin clase
