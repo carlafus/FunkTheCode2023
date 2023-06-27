2b6aeb99dab37f1644fa618b11afa201283d3e1c
DaniloProfita <111090713+DaniloProfita@users.noreply.github.com>
2023-06-26 22:09:09 -0300

    manejo_archivos.py
    Semana 2- Python - 1.1 Introducción a lo que es el manejo de archivos / 1.2 Especificar el juego de caracteres de un archivo de texto

--- tecnicaturagit3Py/Archivos/Leccion02/manejo_archivos.py
+++ tecnicaturagit3Py/Archivos/Leccion02/manejo_archivos.py
@@ -1,14 +1,16 @@
-# declaramos una variable
+# Declaramos una variable
 try:
-    archivo = open('prueba.txt', 'w', encoding = 'utf8')# la w es de write (escribir en ingles)
-    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
-    archivo.write('los acentos son importantes para las palabras\n')
+    archivo = open('prueba.txt', 'w', encoding='utf8')  # La w es de write, utf8 para los acentos
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt. \n')
+    archivo.write('Los acentos son importantes para las palabras\n')
     archivo.write('como por ejemplo: acción, ejecución y producción\n')
-    archivo.write('Con esto terminamos.')
-    archivo.write('# las letras son:\n r read,\n a append anexa,\n w write escribe,\n x crea un archivo')
-    archivo.write('\n t esta es para texto o text, \n b archivos binarios, \n w+ lee y escribe son iguales r+\n')
+    archivo.write('las letras son: \nr read leer, \na append anexa, \nw write escribe, \nx crea un archivo\n')
+    archivo.write('t esta es para texto o text, \nb archivos binarios, \nw+ y r+ leen y escriben \n')
+    archivo.write('Saludos a todos los alumnos de la tecnicatura \n')
+    archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
-finally: # siempre se ejecuta
-    archivo.close() # con esto se debe cerrar el archivo
-    # archivo.write('todo quedo perfecto'): este es un error
+finally:  # Siempre se ejecuta
+    archivo.close()  # Con esto se debe cerrar el carchivo
+
+#archivo.write('Todo quedo perfecto'): es un error porque no abrimos el archivo
